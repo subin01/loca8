f6a5bdf1eaccae1b1c61c4efdb4d87256b10a5ad
Subin Paul <subin.paul@publicissapient.com>
2021-05-10 23:57:57 +1000

    style changes, _document cleanup

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,16 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+  // static async getInitialProps(ctx) {
+  //   const initialProps = await Document.getInitialProps(ctx)
+  //   return { ...initialProps }
+  // }
 
   render() {
     return (
       <Html>
         <Head>
           <meta name="description" content="Locate the owner" />
+          <meta name="theme-color" content="#791aff"></meta>
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;700&display=swap" rel="stylesheet" />
