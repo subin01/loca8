70703d12beb5e06fc0b390fba8500d102377f3ba
Subin Paul <subin.paul@publicissapient.com>
2021-08-25 21:02:32 +1000

    Font, Bg blur fixes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,13 +4,13 @@ export default function Footer() {
       <div className="wrap">
         <div className="logo-line">
           Follow us on
-          <a href="https://www.instagram.com/loca8.me" target="_blank">
+          <a href="https://www.instagram.com/loca8.me" target="_blank" rel="noopener">
             <img src="/images/icon-insta.svg" alt="Instagram" width={30} height={30} />
           </a>
-          <a href="https://www.facebook.com/Loca8-100434038928600" target="_blank">
+          <a href="https://www.facebook.com/Loca8-100434038928600" target="_blank" rel="noopener">
             <img src="/images/icon-fb.svg" alt="Facebook" width={30} height={30} />
           </a>
-          <a href="https://www.linkedin.com/company/loca8" target="_blank">
+          <a href="https://www.linkedin.com/company/loca8" target="_blank" rel="noopener">
             <img src="/images/icon-in.svg" alt="LinkedIn" width={30} height={30} />
           </a>
         </div>
